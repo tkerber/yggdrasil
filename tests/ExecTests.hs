4d4ec0b2a3b655440d93c0111894c21754cb9ed7
Thomas Kerber <t.kerber@ed.ac.uk>
2018-08-29 17:37:52 +0100

    Start looking at testing.

---
+++ tests/ExecTests.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module ExecTests (spec) where
+
+import System.Random
+import Test.Hspec
+import Test.Hspec.QuickCheck
+import Yggdrasil.ExecutionModel
+import Yggdrasil.SimpleState
+
+run' :: RandomGen g => g -> Action SimpleState b -> (Maybe b, g)
+run' = run
+
+spec :: Spec
+spec = do
+    describe "action" $ do
+        prop "obeys return" $ \i (x::String) ->
+            fst (run' (mkStdGen i) (return x)) == Just x
---
+++ tests/Spec.hs
@@ -0,0 +1,6 @@
+import Test.Hspec
+
+import qualified ExecTests
+
+main :: IO ()
+main = hspec ExecTests.spec
